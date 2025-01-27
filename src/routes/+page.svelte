269a03f054ca1107c6e4bc5864aa42d586df82b9
Ripardo <roope.ripatti@gmail.com>
2025-01-27 15:51:52 +0200

    added vercel analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
   import Desktop from '$lib/Desktop.svelte';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit'
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
+  
 </script>
 
 <Desktop />
